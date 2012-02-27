88dd7be76c02aec38e702b1d9c7dd5b75ea35a4f
Jarek Szczepański <imrahil@imrahil.com>
2012-02-27 11:59:01 +0100

    Dodana ikonka. Dodany popup "O programie".

--- 2.package_deploy.bat
+++
@@ -1,2 +0,0 @@
-cls
-c:\Dev\Flex_SDK\blackberry-tablet-sdk-2.0.0\bin\blackberry-airpackager.bat -devMode -debugToken debugtoken_imrahil.bar -package "bin-release\KodyPocztowe.bar" src\KodyPocztowe-app.xml -C bin-release bin-release\KodyPocztowe.swf -C src src\blackberry-tablet.xml -e src\data\kody_pocztowe.sqlite data/kody_pocztowe.sqlite -installApp -launchApp -password jarek -device 192.168.1.10
