e8f4fb205fa5f170da5b885b31318602bebb124f
Jarek Szczepański <imrahil@imrahil.com>
2012-03-03 22:26:03 +0100

    Poprawki w README

--- 2.package.bat
+++ 2.package.bat
@@ -1,3 +1,2 @@
 cls
-call "%PLAYBOOK_HOME%\bin\blackberry-airpackager.bat" -devMode -debugToken debugtoken_imrahil.bar -package "bin-release\KodyPocztowe.bar" src\KodyPocztowe-app.xml -C bin-release bin-release\KodyPocztowe.swf -C src src\blackberry-tablet.xml src\KodyPocztowe-icon.png -e src\data\kody_pocztowe.sqlite data/kody_pocztowe.sqlite 
-rem -installApp -launchApp -password jarek -device 192.168.1.10
+call "%PLAYBOOK_HOME%\bin\blackberry-airpackager.bat" -devMode -debugToken debugtoken_imrahil.bar -package "bin-release\KodyPocztowe.bar" src\KodyPocztowe-app.xml -C bin-release bin-release\KodyPocztowe.swf -C src src\blackberry-tablet.xml src\KodyPocztowe-icon.png -e src\data\kody_pocztowe.sqlite data/kody_pocztowe.sqlite -installApp -launchApp -password jarek -device 192.168.1.10
